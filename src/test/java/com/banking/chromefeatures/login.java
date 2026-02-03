51a16821f85654175aec0eaf568874cda359586f
karshjarak <karshi555@gmail.com>
2026-02-03 13:51:49 +0530

    data

--- src/test/java/com/banking/chromefeatures/login.java
+++ src/test/java/com/banking/chromefeatures/login.java
@@ -1,9 +1,14 @@
 package com.banking.chromefeatures;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class login {
+	
+	ChromeDriver driver;
 
 	// browser commands
 	// get()
@@ -17,24 +22,43 @@ public class login {
 	// getCurrentUrl()
 	// getPageSource()
 	//new test
+	
+	
+	
 
 	@Test
 	public void sampleMethod() throws InterruptedException {
 
-		ChromeDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
 		String title = driver.getTitle();
 		System.out.println("Title of the page: " + title);
 		String url = driver.getCurrentUrl();
 		System.out.println("Current URL: " + url);
+		
+		Assert.assertTrue(title.contains("Facebook"), "Title does not contain Facebook");
+	
+		
+		
 		driver.navigate().to("https://www.instagram.com/");
+		String newurl = driver.getCurrentUrl();
+		
+		
+		Thread.sleep(5000);
+		Assert.assertEquals(newurl, "https://www.facebook.com/", "URL does not match expected value");
 		driver.navigate().back();
 		driver.navigate().forward();
 		driver.navigate().refresh();
 		Thread.sleep(5000);
-		driver.close();
+	
 
+	}
+	
+	
+	@AfterMethod
+	public void sampleMethodTwo() {
+		driver.quit();
 	}
 
 }
