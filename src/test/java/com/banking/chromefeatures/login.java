4af721b09f7edc783ccf7ffa4c568de4684d8403
karshjarak <karshi555@gmail.com>
2026-02-03 13:35:09 +0530

    jh

---
+++ src/test/java/com/banking/chromefeatures/login.java
@@ -0,0 +1,39 @@
+package com.banking.chromefeatures;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class login {
+
+	// browser commands
+	// get()
+	// navigate().to()
+	// navigate().back()
+	// navigate().forward()
+	// navigate().refresh()
+	// close()
+	// quit()
+	// getTitle()
+	// getCurrentUrl()
+	// getPageSource()
+
+	@Test
+	public void sampleMethod() throws InterruptedException {
+
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		System.out.println("Title of the page: " + title);
+		String url = driver.getCurrentUrl();
+		System.out.println("Current URL: " + url);
+		driver.navigate().to("https://www.instagram.com/");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
+		Thread.sleep(5000);
+		driver.close();
+
+	}
+
+}
